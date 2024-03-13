77e9c0a69f69da8b255fdbf0a7be6cc6cc755ff7
Hari Veerendra <veerendrahari396@gmail.com>
2024-03-13 13:42:40 +0530

    final project

--- src/Comp.js
+++ src/Comp.js
@@ -4,17 +4,49 @@ import "./Imgslider_and_home.css";
 import "./Comp.css";
 import Head from "./Menu";
 import ProductInfo from "./ProductInfo";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Foot from "./Foot";
 
 export default function Comp() {
+  const items = [
+    {
+      value: "Watches",
+      link: `/app/${0}`,
+    },
+    {
+      value: "Mobiles",
+      link: `/app/${1}`,
+    },
+    {
+      value: "Shoes",
+      link: `/app/${2}`,
+    },
+    {
+      value: "Phones",
+      link: `/app/${1}`,
+    },
+  ];
   const item = useParams();
   return (
     <>
       <Head />
-      <div>Result</div>
+      <div style={{ color: "black" }}>Result</div>
       <div className="_container">
-        <div className="Category_filter"></div>
+        <div className="Category_filter">
+          <h1 style={{ color: "black", margin: "10px" }}>Products</h1>
+          {items.map((n) => {
+            return (
+              <>
+                <Link
+                  to={n.link}
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  <div id="_items_">{n.value}</div>
+                </Link>
+              </>
+            );
+          })}
+        </div>
         <div id="packContainer">
           {ProductInfo[item.item].map((e) => {
             return <Product item={item.item} Id={e} />;
